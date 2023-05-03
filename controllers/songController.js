4eaf6c716fdf3171d58a25d4a9bbb9bd31fd5f04
kaleabbyh <kaleabbyh@gmail.com>
2023-05-03 22:42:40 +0300

    song controller updated

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,21 +1,34 @@
 const asyncHandler = require("express-async-handler");
-require("dotenv").config();
 const Song = require("../models/songModel");
 
 // @desc    add new song
 // @route   POST /api/song/addSong
 // @access  Public
 const addSong = asyncHandler(async (req, res) => {
-  console.log("new song added");
-  res.send("new song added");
+  const { title, description } = req.body;
+  if (!title) {
+    res.send("please add song title");
+  }
+  const song = await Song.create(req.body);
+  if (song) {
+    res.status(201).json(song);
+  } else {
+    res.status(400);
+    throw new Error("Invalid song data");
+  }
 });
 
 // @desc    get all songs
 // @route   POST /api/song/getAllSongs
 // @access  Public
 const getAllSongs = asyncHandler(async (req, res) => {
-  console.log("all songs");
-  res.send("all songs");
+  const songs = await Song.find();
+  if (songs) {
+    res.status(201).json(songs);
+  } else {
+    res.status(400);
+    throw new Error("no song data");
+  }
 });
 
 // @desc   edit song
